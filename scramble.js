b33be463fa9313e910553f506093f30cb1bb70a7
Hackbright Student <info@hackbrightacademy.com>
2015-07-14 17:36:46 -0700

    Finished word-scramble game

--- scramble.js
+++ scramble.js
@@ -7,19 +7,28 @@ while (playing) {
 
     // Grab a random word from the WORDS array
     var randindex = Math.floor(Math.random() * WORDS.length);
-    console.log(randindex);
-    // Grab the corresponding scrambled word from the SCRAMBLED array
-
-    // prompt the user to guess the original word when given the scrambled word
-
-    // If the user guesses wrong, tell them they were wrong and the right word.
+    var word = WORDS[randindex];
+    console.log("line11", word);
     
-    // If the user guesses the correct word, congratulate them.
 
+    // Grab the corresponding scrambled word from the SCRAMBLED array
+    var scram = SCRAMBLED[randindex];
+        console.log("line16", scram);
+    
+    // prompt the user to guess the original word when given the scrambled word
+    var guess = prompt("Unscramble this word: ", scram);
+    // If the user guesses wrong, tell them they were wrong and the right word.
+    if (guess !== word) {
+        alert("YOU'RE WRONG!!!! TRY AGAIN!!!!");
+    }
+    // If the user guesses the correct word, congratulate them.
+    if (guess === word) {
+        alert("Congrats, You're right!");
+    }
     playing = confirm("Press OK To keep playing or Cancel to quit");
 
     // What does confirm() return?
     // What does setting the playing variable do?
 }
 
-// alert("goodbye!");
+alert("goodbye!");
